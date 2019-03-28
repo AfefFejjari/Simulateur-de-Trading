f834ba9d399978a8d438cf19135cb13f06629cf9
AfefFejjari <47954179+AfefFejjari@users.noreply.github.com>
2019-03-28 12:22:57 +0100

    Add files via upload

---
+++ spring-Market/src/main/java/com/trading/jwtauthentication/model/RoleName.java
@@ -0,0 +1,6 @@
+package com.trading.jwtauthentication.model;
+
+public enum  RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ spring-Market/src/main/java/com/trading/jwtauthentication/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.trading.jwtauthentication.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.trading.jwtauthentication.model.Role;
+import com.trading.jwtauthentication.model.RoleName;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
