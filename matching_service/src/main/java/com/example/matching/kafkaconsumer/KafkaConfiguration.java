d5b29dbcdaea4bf6a7bf6d330e03d6832bfcb476
AfefFejjari <47954179+AfefFejjari@users.noreply.github.com>
2019-05-15 10:21:52 +0200

    Add files via upload

--- matching_service/src/main/java/com/example/matching/kafkaconsumer/KafkaConfiguration.java
+++ matching_service/src/main/java/com/example/matching/kafkaconsumer/KafkaConfiguration.java
@@ -1,62 +1,63 @@
 package com.example.matching.kafkaconsumer;
 
-
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.catalina.User;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import com.example.matching.model.Order;
-import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
 
 @EnableKafka
 @Configuration
 public class KafkaConfiguration {
 
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> config = new HashMap<>();
 
-	 @Bean
-	    public ConsumerFactory<String, String> consumerFactory() {
-	        Map<String, Object> config = new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 
-	        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:51539");
-	        config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
-	        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-	        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-
-	        return new DefaultKafkaConsumerFactory<>(config);
-	    }
-
-
-	    @Bean
-	    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
-	        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory();
-	        factory.setConsumerFactory(consumerFactory());
-	        return factory;
-	    }
+        return new DefaultKafkaConsumerFactory<>(config);
+    }
 
 
-	    @Bean
-	    public ConsumerFactory<String, Order> orderConsumerFactory() {
-	        Map<String, Object> config = new HashMap<>();
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
 
-	        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-	        config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_json");
-	        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-	        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-	        return new DefaultKafkaConsumerFactory<>(config , new StringDeserializer(), new JsonDeserializer<>(Order.class));
-	    }
 
-	    @Bean
-	    public ConcurrentKafkaListenerContainerFactory<String, Order> userKafkaListenerFactory() {
-	        ConcurrentKafkaListenerContainerFactory<String, Order> factory = new ConcurrentKafkaListenerContainerFactory<>();
-	        factory.setConsumerFactory(orderConsumerFactory());
-	        return factory;
-	    }
+    @Bean
+    public ConsumerFactory< String,  Order> userConsumerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_json");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs, new StringDeserializer(),
+                new JsonDeserializer<>(Order.class));
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Order> userKafkaListenerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Order> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(userConsumerFactory());
+        return factory;
+    }
+
 }
